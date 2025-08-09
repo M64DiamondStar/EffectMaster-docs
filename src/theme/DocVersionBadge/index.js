26345ec56afb311c475798dd961825ab4ebe8d81
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2025-08-09 16:21:46 +0200

    Improve version badge to fit more with the theme

---
+++ src/theme/DocVersionBadge/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import clsx from 'clsx';
+import Translate from '@docusaurus/Translate';
+import {ThemeClassNames} from '@docusaurus/theme-common';
+import {useDocsVersion} from '@docusaurus/plugin-content-docs/client';
+export default function DocVersionBadge({className}) {
+  const versionMetadata = useDocsVersion();
+  if (versionMetadata.badge) {
+    return (
+      <span
+        className={clsx(
+          className,
+          ThemeClassNames.docs.docVersionBadge,
+          'badge',
+        )}>
+          <u>
+            <Translate
+              id="theme.docs.versionBadge.label"
+              values={{versionLabel: versionMetadata.label}}>
+              {'Version: {versionLabel}'}
+            </Translate>
+          </u>
+      </span>
+    );
+  }
+  return null;
+}
