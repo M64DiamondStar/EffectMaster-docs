53e674ed6c878cd1a34cfabe05fc84318a636ec5
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2025-08-09 11:12:14 +0200

    Fixed dark mode and cleaned up docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,149 +1,60 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
 import {themes as prismThemes} from 'prism-react-renderer';
-
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'EffectMaster Docs',
-  tagline: 'Create beautiful visuals for your Minecraft Server',
-  favicon: '/img/favicon.ico',
+    title: 'EffectMaster Docs',
+    tagline: 'Create beautiful visuals for your Minecraft Server',
+    favicon: '/img/favicon.ico',
+    future: {v4: true},
+    url: 'https://effectmaster.m64.dev/',
+    baseUrl: '/',
+    organizationName: 'M64DiamondStar',
+    projectName: 'EffectMaster-docs',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    i18n: {defaultLocale: 'en', locales: ['en']},
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-  future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
-
-  // Set the production url of your site here
-  url: 'https://effectmaster.m64.dev/',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'M64DiamondStar', // Usually your GitHub org/user name.
-  projectName: 'EffectMaster-docs', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
-
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/M64DiamondStar/EffectMaster-docs/tree/master/',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      }),
+    presets: [
+        [
+            'classic',
+            ({
+                docs: {
+                    sidebarPath: './sidebars.js',
+                    editUrl: 'https://github.com/M64DiamondStar/EffectMaster-docs/tree/master/',
+                },
+                theme: {customCss: './src/css/custom.css'},
+            }),
+        ],
     ],
-  ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: 'img/effectmaster-social-card.jpg',
-      navbar: {
-        title: 'EffectMaster Docs',
-        logo: {
-          alt: 'EffectMaster logo',
-          src: 'img/logo.png',
+    themeConfig: {
+        image: 'img/effectmaster-social-card.png',
+        navbar: {
+            title: 'EffectMaster Docs',
+            logo: {alt: 'EffectMaster logo', src: 'img/logo.png'},
+            items: [
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'sidebar',
+                    position: 'left',
+                    label: 'Documentation',
+                    autoCollapseCategories: false,
+                    hideable: true,
+                },
+                {href: 'https://github.com/M64DiamondStar/EffectMaster', label: 'GitHub', position: 'right'},
+            ],
         },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'sidebar',
-            position: 'left',
-            label: 'Documentation',
-            autoCollapseCategories: false,
-            hideable: true,
-          },
-          {
-            href: 'https://github.com/M64DiamondStar/EffectMaster',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      themeConfig: {
         colorMode: {
-          defaultMode: 'dark',
-          respectPrefersColorScheme: true,
-        }
-      },
-      /*footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },*/
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-        additionalLanguages: ['java']
-      },
-    }),
+            defaultMode: 'dark',
+            disableSwitch: false,
+            respectPrefersColorScheme: false,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+            additionalLanguages: ['java'],
+        },
+    },
 };
 
 export default config;
