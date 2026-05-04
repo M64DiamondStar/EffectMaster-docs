160d7dae5d16913d1dc6b3cb32eaf8556ae20b40
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2026-05-04 15:52:12 +0200

    Added discord invite to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,33 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import {Icon} from '@iconify/react';
+import {useEffect, useState} from 'react';
 
 import styles from './index.module.css';
+
+const DISCORD_INVITE = 'https://discord.com/invite/Scv9afJwXp';
+const DISCORD_GUILD_ID = '1061267552661155891';
+
+function DiscordOnlineCount() {
+    const [online, setOnline] = useState(null);
+
+    useEffect(() => {
+        fetch(`https://discord.com/api/guilds/${DISCORD_GUILD_ID}/widget.json`)
+            .then(res => res.json())
+            .then(data => setOnline(data.presence_count))
+            .catch(() => setOnline(null));
+    }, []);
+
+    if (online === null) return null;
+
+    return (
+        <span className={styles.discordOnline}>
+            <span className={styles.onlineDot}/>
+            {online.toLocaleString()} online
+        </span>
+    );
+}
 
 function HomepageHeader() {
     return (
@@ -15,8 +40,16 @@ function HomepageHeader() {
                     <Link className="button button--secondary button--lg" to="/docs/getting-started">
                         Get Started
                     </Link>
-                    <Link className="button button--secondary button--outline button--lg" to="https://modrinth.com/plugin/effectmaster">
+                    <Link className="button button--secondary button--outline button--lg"
+                          to="https://modrinth.com/plugin/effectmaster">
                         Download
+                    </Link>
+                </div>
+                <div className={styles.buttons}>
+                    <Link className={clsx('button button--lg', styles.discordButton)} to={DISCORD_INVITE}>
+                        <Icon icon="ic:baseline-discord" className={styles.discordIcon}/>
+                        Discord
+                        <DiscordOnlineCount/>
                     </Link>
                 </div>
             </div>
